9a6ad4427e9722f653e110de0e46aef543a6d97d
Jack Guinane <jackguinane@gmail.com>
2021-04-01 21:59:07 -0700

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,62 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Bookcase',
+  siteDescription: 'Locates where I put my books',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        route: '/book/:title',
+        remark: {
+          plugins: [
+            ['@gridsome/remark-prismjs', {transformInlineCode: true}]
+          ]
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      }
+    },
+    {
+      use: 'gridsome-plugin-feed',
+      options: {
+        contentTypes: ['Post'],
+        feedOptions: {
+          title: 'Bookcase',
+          description: 'Not really a blog, but there is still a feed I guess.'
+        },
+        rss: {
+          enabled: true,
+          output: '/feed.xml'
+        },
+        atom: {
+          enabled: false,
+          output: '/feed.atom'
+        },
+        json: {
+          enabled: false,
+          output: '/feed.json'
+        },
+        maxItems: 25,
+        htmlFields: ['description', 'content'],
+        enforceTrailingSlashes: false,
+        filterNodes: (node) => true,
+        nodeToFeedItem: (node) => ({
+          title: node.title,
+          date: node.date || node.fields.date,
+          content: node.content
+        })
+      }
+    }
+  ]
+}
