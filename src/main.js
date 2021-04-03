82ba0622fc40b75a4afcb9fdc719b44f0a682dc4
Jack Guinane <jackguinane@gmail.com>
2021-04-03 12:12:56 -0700

    add focus-visible

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import FocusVisible from 'vue-focus-visible'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+	Vue.use(FocusVisible)
 	// Set default layout as a global component
 	Vue.component('Layout', DefaultLayout)
 	head.htmlAttrs = { lang: 'en' }
