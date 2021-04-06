8e69f7eef3d1308a628812d2372cb8f9d5a4c214
Jack Guinane <jackguinane@gmail.com>
2021-04-05 20:31:15 -0700

    fix focus visible

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -1,5 +1,5 @@
 <template>
-	<g-link :to="'/'" class="search-button">
+	<g-link :to="'/'" class="search-button" v-focus-visible>
 		<svg class="icon" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.537 34.109">
 			<path d="M4.02 16.141a10 10 0 0119.405-4.838 9.926 9.926 0 01-.584 6.526l11.151 6.7-1.212 2.017-11.15-6.7a10 10 0 01-13.058 2.448 9.935 9.935 0 01-4.553-6.153zm7.852-9.838a7.647 7.647 0 103.7 14.839 7.647 7.647 0 10-3.7-14.84z"/>
 		</svg>
